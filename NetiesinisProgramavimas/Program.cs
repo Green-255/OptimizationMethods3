edd856096ae9bb76c78b9bdd452d2be946102581
nei nei <naglis.kontautas04@gmail.com>
2024-12-15 02:14:06 +0200

    added a new file witch working optimization

--- NetiesinisProgramavimas/Program.cs
+++ NetiesinisProgramavimas/Program.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.VisualBasic;
+using System.Drawing;
 using System.Numerics;
+using Plotly.NET;
+using Plotly.NET.LayoutObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Plotly.NET.StyleParam;
+
 
 class Program()
 {
-    public const double Epsilon = 1e-6;
+    public const double Epsilon = 1e-2;
     public static void Main(string[] args)
     {
         double x = 0;
@@ -16,59 +24,159 @@ class Program()
 
         //x = 0.5;
         //y = 0.8;
-        //z = 0.0;
+        //z = 0;
 
-        double reductionFactor = 0.05;
-        double r = 10;
-        Func<double, double, double, double, double> penaltyFunc = (x, y, z, r) => PenaltyFunction(x, y, z, r);
+        //double r = 1;
+        //Func<double, double, double, double, double> targetFunctionFunc = (x, y, z, r) => TargetFunctionWithPenalty(x, y, z, r);
 
-        var (pointTimeline, functionCalls, cycles) = FastestDescentWithPenalty(x, y, z, r, penaltyFunc, reductionFactor);
+        ////var (pointTimeline, functionCalls, cycles) = FastestDescentWithPenalty(x, y, z, r, targetFunctionFunc);
+        //var (pointTimeline, functionCalls, cycles) = GradientDescentWithPenalty(x, y, z, r, targetFunctionFunc);
 
-        foreach (var point in pointTimeline)
-        {
-            Console.WriteLine($"x= {point.Item1}, y= {point.Item2}, z= {point.Item3}, lr= {point.Item4}");
-        }
-        Console.WriteLine($"Function Calls: {functionCalls}, Cycles: {cycles}");
+        //Console.WriteLine("==========================================");
+        //int count = 0;
+        //foreach (var point in pointTimeline)
+        //{
+        //    Console.WriteLine($"\n{count} ({point.Item1}, {point.Item2}, {point.Item3}), lr: {point.Item4}, f: {point.Item5}");
+        //    count++;
+        //}
+        //Console.WriteLine($"Function Calls: {functionCalls}, Cycles: {cycles}");
+
+        TranslateFromPython.Mainpiton(x,y,z,1,5);
     }
 
-    public static (List<(double, double, double, double)>, int, int) FastestDescentWithPenalty(
-        double x, double y, double z, double r, Func<double, double, double, double, double> penaltyFunc, double redFactor=0.1)
+    public static void GradientDescentWithPenalty(
+        double x, double y, double z, double r, Func<double, double, double, double, double> targetFunctionFunc)
     {
-        List<(double, double, double, double)> pointTimeLine = new();
-        pointTimeLine.Add((x, y, z, 1));
+        List<(double, double, double, double, double)> pointTimeLine = new();
+        pointTimeLine.Add((x, y, z, 1, targetFunctionFunc(x, y, z, r)));
         int cycles = 0;
-        int iteration = 0;
+        int iterations = 0;
         int targetFunctionCalled = 0;
+        double lr = 0.01;
+        double old_b = PenaltyFunction(x,y,z,r);
 
-        while (iteration < 500 && r >= 0)
+        while (true)
         {
-            Console.WriteLine($"Iteration {iteration + 1}, Penalty Multiplier: {r}");
-            (double gradX, double gradY, double gradZ) = PartialGradientsWithPenalty(x, y, z, r);
-            targetFunctionCalled += 6; // 3 gradinets, each with equality and inequality constraints
 
-            if (Math.Abs(gradX) < Epsilon && Math.Abs(gradY) < Epsilon && Math.Abs(gradZ) < Epsilon)
+            (double gradX, double gradY, double gradZ) = PartialGradientsWithPenalty(x,y,z,r);
+
+            double b = PenaltyFunction(x,y,z,r);
+            targetFunctionCalled++;
+
+            x -= lr * gradX;
+            y -= lr * gradY;
+            z -= lr * gradZ;
+            Console.WriteLine($"{iterations}| ({x}, {y}, {z}) | {b}");
+            r /= 2;
+            lr /= 2;
+            cycles++;
+
+            if (old_b < b)
             {
-                Console.WriteLine($"Stopped. gradX: {gradX}, gradY: {gradY}, gradZ: {gradZ}");
-                break;
+                iterations++;
+                if(iterations >= 5)
+                {
+                    Console.WriteLine($"Mehtod stoped due to b getting worse");
+                    break;
+                }
             }
+            old_b = b;
+        }
 
-            Func<double, double> goldenSectionFunc = lambda => penaltyFunc(x - lambda * gradX, y - lambda * gradY, z - lambda * gradZ, r);
+        //var last = pointTimeLine.Last();
+        //Console.WriteLine($"{last.Item1}, {last.Item2}, {last.Item3}, {targetFunctionCalled * 6}, {cycles}");
+    }
 
-            double lambda = GoldenSectionSearch(goldenSectionFunc, ref cycles, ref targetFunctionCalled);
+    public static (List<(double, double, double, double, double)>, int, int) FastestDescentWithPenalty(
+        double x, double y, double z, double r, Func<double, double, double, double, double> targetFunctionFunc)
+    {
+        List<(double, double, double, double, double)> pointTimeLine = new();
+        pointTimeLine.Add((x, y, z, 1, targetFunctionFunc(x, y, z, r)));
+        int cycles = 0;
+        int iterations = 0;
+        int targetFunctionCalled = 0;
+        double old_f = PenaltyFunction(x, y, z, r);
+
+        while (iterations < 20)
+        {
+            (double gradX, double gradY, double gradZ) = PartialGradientsWithPenalty(x, y, z, r);
+            targetFunctionCalled++;
+            Func<double, double> targetFunctionValueFunc = lambda => targetFunctionFunc(x - lambda * gradX, y - lambda * gradY, z - lambda * gradZ, r);
+
+            double lambda = GoldenSectionSearch(targetFunctionValueFunc, ref cycles, ref targetFunctionCalled);
+            //double lambda = HalfSectionSearch(targetFunctionValueFunc, ref cycles, ref targetFunctionCalled);
+
+            //Console.WriteLine(x < lambda * gradX);
             x -= lambda * gradX;
             y -= lambda * gradY;
             z -= lambda * gradZ;
-
-            pointTimeLine.Add((x, y, z, lambda));
+            r /= 5;
             cycles++;
-            iteration++;
-            r -= redFactor;
+            iterations++;
+
+            double new_f = PenaltyFunction(x, y, z, r);
+
+            if (iterations == 30){
+                break;
+            }
+            //if (old_f < new_f)
+            //{
+            //    Console.WriteLine($"\n\nold_f < new_f: {old_f < new_f} | {old_f} < {new_f}");
+            //    Console.WriteLine($"{iterations}:, {x}, {y}, {z}, r: {r} | {new_f} , {TargetFunctionWithPenalty(x,y,z,r)}\n");
+            //    break;
+            //}
+
+            //if (Math.Abs(EqualityConstraintValue(x,y,z)) < Epsilon) {
+            //    Console.WriteLine($"\n Eq= {EqualityConstraintValue(x,y,z)} maziau nei Epsilon\n");
+            //    break;
+            //}
+            old_f = new_f;
+            Console.WriteLine($"\n{iterations}:, {x}, {y}, {z}, r: {r} | {new_f}");
+            pointTimeLine.Add((x, y, z, lambda, TargetFunctionNotMinimized(x, y, z)));
+
         }
 
-        return (pointTimeLine, targetFunctionCalled, cycles);
+        return (pointTimeLine, targetFunctionCalled * 6, cycles);
     }
 
-    private static double GoldenSectionSearch(Func<double, double> func, ref int cycles, ref int targetFunctionCalled, double leftBorder = 0, double rightBorder = 5)
+
+    private static double HalfSectionSearch(Func<double, double> targetFunction,
+        ref int cycles, ref int targetFunctionCalled, double leftBorder = 0, double rightBorder = 2)
+    {
+        double interval = rightBorder - leftBorder;
+        double middlePoint = (leftBorder + rightBorder) / 2;
+        double middlePointValue = targetFunction(middlePoint);
+        double lambda = middlePoint;
+
+        while (interval > Epsilon)
+        {
+            double leftPoint = leftBorder + interval / 4;
+            double leftPointValue = targetFunction(leftPoint);
+            targetFunctionCalled++;
+            if (leftPointValue < middlePointValue)
+            {
+                rightBorder = middlePoint;
+                middlePoint = leftPoint;
+                middlePointValue = leftPointValue;
+            }
+            else
+            {
+                double rightPoint = rightBorder - interval / 4;
+                double rightPointValue = targetFunction(rightPoint);
+                targetFunctionCalled++;
+                leftBorder = middlePoint;
+                middlePoint = rightPoint;
+                middlePointValue = rightPointValue;
+            }
+            interval = rightBorder - leftBorder;
+            lambda = middlePoint;
+            cycles++;
+        }
+        return lambda;
+    }
+
+    private static double GoldenSectionSearch(Func<double, double> func,
+        ref int cycles, ref int targetFunctionCalled, double leftBorder = 0, double rightBorder = 4)
     {
         double T = (-1 + Math.Sqrt(5)) / 2;
         double interval = rightBorder - leftBorder;
@@ -77,7 +185,7 @@ class Program()
 
         double f1 = func(lambda1);
         double f2 = func(lambda2);
-        targetFunctionCalled += 12;
+        targetFunctionCalled += 2;
 
         while (interval > Epsilon)
         {
@@ -89,7 +197,6 @@ class Program()
                 interval = rightBorder - leftBorder;
                 lambda2 = leftBorder + T * interval;
                 f2 = func(lambda2);
-                targetFunctionCalled += 6;
             }
             else
             {
@@ -99,7 +206,6 @@ class Program()
                 interval = rightBorder - leftBorder;
                 lambda1 = rightBorder - T * interval;
                 f1 = func(lambda1);
-                targetFunctionCalled += 6;
             }
             cycles++;
             targetFunctionCalled++;
@@ -108,41 +214,78 @@ class Program()
         return (f1 < f2) ? lambda1 : lambda2;
     }
 
+    //public static (double, double, double) PartialGradientsWithPenalty(double x, double y, double z, double r)
+    //{
+    //    (double gradientX, double gradientY, double gradientZ) = PartialGradients(x, y, z);
+
+    //    double equalityConstraint = EqualityConstraintValue(x, y, z);
+    //    double ecX = equalityConstraint * 4 * (y + z);
+    //    double ecY = equalityConstraint * 4 * (x + z);
+    //    double ecZ = equalityConstraint * 4 * (x + y);
+
+    //    double icX = x < 0 ? (2 * x) : 0;
+    //    double icY = y < 0 ? (2 * y) : 0;
+    //    double icZ = z < 0 ? (2 * z) : 0;
+
+    //    gradientX += (icX + ecX) / r;
+    //    gradientY += (icY + ecY) / r;
+    //    gradientZ += (icZ + ecZ) / r;
+
+    //    return (gradientX, gradientY, gradientZ);
+    //}
     public static (double, double, double) PartialGradientsWithPenalty(double x, double y, double z, double r)
     {
-        double gradX_f = y * z;
-        double gradY_f = x * z;
-        double gradZ_f = x * y;
+        (double gradientX, double gradientY, double gradientZ) = PartialGradients(x, y, z);
 
-        //Console.WriteLine($"{gradX_f}, {gradY_f}, {gradZ_f}");
+        double equalityConstraint = EqualityConstraintValue(x, y, z);
+        double ecX = 2 * Math.Abs(y + z);
+        double ecY = 2 * Math.Abs(x + z);
+        double ecZ = 2 * Math.Abs(x + y);
 
-        double gradX_g = 2 * (y + z);
-        double gradY_g = 2 * (x + z);
-        double gradZ_g = 2 * (x + y);
-        //Console.WriteLine($"{gradX_g}, {gradY_g}, {gradZ_g}");
+        double icX = x < 0 ? x : 0;
+        double icY = y < 0 ? y : 0;
+        double icZ = z < 0 ? z : 0;
 
-        double gradX = gradX_f + 2 * r * EqualityConstraintValue(x, y, z) * gradX_g;
-        double gradY = gradY_f + 2 * r * EqualityConstraintValue(x, y, z) * gradY_g;
-        double gradZ = gradZ_f + 2 * r * EqualityConstraintValue(x, y, z) * gradZ_g;
-        //Console.WriteLine($"{gradZ}, {gradY}, {gradZ}");
+        gradientX += (icX + ecX) / r;
+        gradientY += (icY + ecY) / r;
+        gradientZ += (icZ + ecZ) / r;
 
-        return (gradX, gradY, gradZ);
-    }
-    public static (double, double, double) PartialGradients(double x, double y, double z)
-    {
-        return (y * z, x * z, x * y);
+        return (gradientX, gradientY, gradientZ);
     }
 
-    public static double PenaltyFunction(double x, double y, double z, double r)
+    public static double TargetFunctionWithPenalty(double x, double y, double z, double r)
     {
-        double target = TargetFunction(x, y, z);
-        double equalityPenalty = Math.Pow(EqualityConstraintValue(x, y, z), 2);
+        return TargetFunction(x, y, z) + PenaltyFunction(x, y, z, r);
+    }
 
-        // might be incorrect due to MIN(0, x) instead of MAX(0, x) or even it should be a different method
-        double inequalityPenalty = Math.Pow(Math.Min(0, x), 2) + Math.Pow(Math.Min(0, y), 2) + Math.Pow(Math.Min(0, z), 2); 
-        //double inequalityPenalty = Pow(Min(x, 0),2) + Pow(Min(y, 0), 2) + Pow(Min(z, 0), 2);
+    //private static double PenaltyFunction(double x, double y, double z, double r)
+    //{
+    //    double equalityConstraint = Math.Pow(EqualityConstraintValue(x, y, z), 2);
+    //    Func<double, double> inequalityFunc = (x) => (Math.Pow(AvoidNegative(x), 2));
+    //    double inequalityConstraint = inequalityFunc(x) + inequalityFunc(y) + inequalityFunc(z);
 
-        return target + r * (equalityPenalty + inequalityPenalty);
+    //    return (1 / r) * (equalityConstraint + inequalityConstraint);
+    //}
+    private static double PenaltyFunction(double x, double y, double z, double r)
+    {
+        double equalityConstraint = Math.Abs(EqualityConstraintValue(x, y, z));
+        Func<double, double> inequalityFunc = (x) => AvoidNegative(x);
+        double inequalityConstraint = inequalityFunc(x) + inequalityFunc(y) + inequalityFunc(z);
+
+        return (1 / r) * (equalityConstraint + inequalityConstraint);
+    }
+
+    public static double EqualityConstraintValue(double x, double y, double z)
+    {
+        return (x * y + y * z + x * z) * 2 - 1;
+    }
+
+    public static double AvoidNegative(double x)
+    {
+        //Console.WriteLine($"{x}| {Math.Abs(Math.Min(x, 0))}, {Math.Min(x, 0)}, {Math.Max(-x, 0)}");
+        //return Math.Abs(Math.Min(x, 0));
+        //return Math.Min(x, 0);
+        return Math.Max(-x, 0);
     }
 
     public static double TargetFunction(double x, double y, double z)
@@ -150,23 +293,66 @@ class Program()
         return -x * y * z;
     }
 
-    public static double EqualityConstraintValue(double x, double y, double z)
+    public static double TargetFunctionNotMinimized(double x, double y, double z)
     {
-        //x = Math.Abs(x);
-        //y = Math.Abs(y);
-        //z = Math.Abs(z);
-        return (x * y + y * z + x * z) * 2 - 1;
-        //return Math.Abs((x*y + y*z + x*z) *2 -1) < Epsilon;
+        return x * y * z;
     }
 
-    public static (bool, double, double, double) NegativeConstarint(double x, double y, double z)
+    public static (double, double, double) PartialGradients(double x, double y, double z)
     {
-        double nx = x > 0 ? x : 0;
-        double ny = y > 0 ? y : 0;
-        double nz = z > 0 ? z : 0;
-        bool isConstraintViolated = x >= 0 && y >= 0 && z >= 0;
-
-        return (isConstraintViolated, nx, ny, nz);
+        return (-y * z, -x * z, -x * y);
     }
+
+    //public static (List<(double, double, double, double, double)>, int, int) GradientDescentWithPenalty(
+    //    double x, double y, double z, double r, Func<double, double, double, double, double> targetFunctionFunc)
+    //{
+    //    List<(double, double, double, double, double)> pointTimeLine = new();
+    //    pointTimeLine.Add((x, y, z, 1, targetFunctionFunc(x, y, z, r)));
+    //    int cycles = 0;
+    //    int iterations = 0;
+    //    int targetFunctionCalled = 0;
+    //    double old_f = PenaltyFunction(x, y, z, r);
+    //    double learningRate = 0.1;
+
+    //    while (true)
+    //    {
+    //        double new_f = PenaltyFunction(x, y, z, r);
+    //        if (new_f > old_f)
+    //        {
+    //            Console.WriteLine($"old_f < new_f: {old_f} < {new_f}");
+    //        }
+    //        old_f = new_f;
+    //        //if (!(r > Epsilon || x < 0 || y < 0 || z < 0))
+    //        //{
+    //        //    Console.WriteLine($"Stopped. r: {r}, x: {x}, y: {y}, z: {z}");
+    //        //    break;
+    //        //}
+    //        (double gradX, double gradY, double gradZ) = PartialGradientsWithPenalty(x, y, z, r);
+    //        targetFunctionCalled++;
+
+    //        x -= learningRate * gradX;
+    //        y -= learningRate * gradY;
+    //        z -= learningRate * gradZ;
+
+    //        pointTimeLine.Add((x, y, z, 1, targetFunctionFunc(x, y, z, r)));
+    //        Console.WriteLine($"Iteration: {iterations}, x: {x}, y: {y}, z: {z}, r: {r}\n");
+
+    //        if (iterations > 5)
+    //        {
+    //            r = Math.Max(r / 5, Epsilon);
+    //            learningRate /= 5;
+    //        }
+
+    //        if (Math.Sqrt(gradX * gradX + gradY * gradY + gradZ * gradZ) < Epsilon)
+    //        {
+    //            break;
+    //        }
+
+    //        cycles++;
+    //        iterations++;
+    //    }
+
+    //    return (pointTimeLine, targetFunctionCalled * 3, cycles);
+    //}
 }
 
---
+++ NetiesinisProgramavimas/TranslateFromPython.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class TranslateFromPython
+{
+    public static void Mainpiton(double a, double b, double c, double r, double rChange)
+    {
+        var (x, y, z, cycles, functionCalls) = OptimizeFunction((a, b, c), r, rChange);
+        Console.WriteLine($"Result: x = {x}, y = {y}, z = {z}\nFunkcijos kvietimų: {functionCalls}, Ciklų kiekis: {cycles}");
+        Console.WriteLine($"{TargetFunctionNotMinimized(x, y, z)}");
+    }
+
+    public static double Distance((double, double, double) point1, (double, double, double) point2)
+    {
+        return Math.Sqrt(
+            Math.Pow(point2.Item1 - point1.Item1, 2) +
+            Math.Pow(point2.Item2 - point1.Item2, 2) +
+            Math.Pow(point2.Item3 - point1.Item3, 2)
+        );
+    }
+
+    public static (double, double, double, int, int) OptimizeFunction((double, double, double) start, double r, double rChange)
+    {
+        double x = start.Item1;
+        double y = start.Item2;
+        double z = start.Item3;
+
+        double lr = 0.01;
+        double epsilon = 1e-6;
+        int functionCalls = 0;
+        int cycles = 0;
+        int i = 0;
+        double old_b = Int32.MaxValue;
+
+        while (true)
+        {
+            Console.WriteLine($"{x} & {y} & {z} \\\\");
+            //Console.WriteLine($"{r} & {old_b} \\\\");
+
+            (x, y, z, int cyclesAddition, int functionCallsAddition) = GradientDescent((x, y, z), lr, epsilon, r);
+            functionCalls += functionCallsAddition;
+            cycles += cyclesAddition;
+            i++;
+
+            r /= rChange;
+            epsilon /= rChange;
+            lr /= rChange;
+
+            double new_b = PenaltyFunction(x, y, z, r);
+            functionCalls += 2;
+            if (old_b <= new_b)
+            {
+                break;
+            }
+            old_b = new_b;
+        }
+
+        return (x, y, z, cycles, functionCalls);
+    }
+
+    public static (double, double, double, int, int) GradientDescent((double, double, double) X, double lr, double epsilon, double r)
+    {
+        int cycles = 0;
+        int functionCalls = 0;
+
+        var (x, y, z) = (X.Item1, X.Item2, X.Item3);
+        var (newX, newY, newZ) = (x, y, z);
+
+        do
+        {
+            x = newX;
+            y = newY;
+            z = newZ;
+
+            (double gradX, double gradY, double gradZ) = PartialGradientsWithPenalty(x, y, z, r);
+            newX = x - lr * gradX;
+            newY = y - lr * gradY;
+            newZ = z - lr * gradZ;
+
+            functionCalls += 8;
+            cycles++;
+        } while (Distance((x, y, z), (newX, newY, newZ)) >= epsilon);
+
+        return (newX, newY, newZ, cycles, functionCalls);
+    }
+
+    private static double PenaltyFunction(double x, double y, double z, double r)
+    {
+        Func<double, double> inequalityFunc = (x) => (Math.Pow(NegativePenaklty(x), 2));
+        double equalityConstraint = Math.Pow(EqualityConstraintValue(x, y, z), 2);
+        double inequalityConstraint = inequalityFunc(x) + inequalityFunc(y) + inequalityFunc(z);
+
+        return (1 / r) * (inequalityConstraint + equalityConstraint);
+    }
+
+
+    public static (double, double, double) PartialGradientsWithPenalty(double x, double y, double z, double r)
+    {
+        double equalityConstraint = EqualityConstraintValue(x, y, z);
+        double ecX = equalityConstraint * 4 * (y + z);
+        double ecY = equalityConstraint * 4 * (x + z);
+        double ecZ = equalityConstraint * 4 * (x + y);
+
+        double icX = x < 0 ? (2 * x) : 0;
+        double icY = y < 0 ? (2 * y) : 0;
+        double icZ = z < 0 ? (2 * z) : 0;
+
+        (double gradientX, double gradientY, double gradientZ) = PartialGradients(x, y, z);
+
+        gradientX += (icX + ecX) / r;
+        gradientY += (icY + ecY) / r;
+        gradientZ += (icZ + ecZ) / r;
+
+        return (gradientX, gradientY, gradientZ);
+    }
+
+    public static double EqualityConstraintValue(double x, double y, double z)
+    {
+        return (x * y + y * z + x * z) * 2 - 1;
+    }
+
+    public static (double, double, double) PartialGradients(double x, double y, double z)
+    {
+        return (-y * z, -x * z, -x * y);
+    }
+    public static double NegativePenaklty(double x)
+    {
+        return Math.Max(-x, 0);
+    }
+    public static double TargetFunctionNotMinimized(double x, double y, double z)
+    {
+        return x * y * z;
+    }
+}
